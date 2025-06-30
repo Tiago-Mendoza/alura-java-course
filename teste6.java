7c95f50d18d83f2c121ea891a8d7a134baac77af
Tiago-Mendoza <mendozatiago21@gmail.com>
2025-06-30 17:48:22 -0300

    Add files via upload

---
+++ teste6.java
@@ -0,0 +1,15 @@
+public class teste6 {
+    public static void main(String[] args){
+        int numero = 7;
+
+        if(numero % 2 == 0) {
+            System.out.println("o numero é par " + numero);
+        
+        }else {
+            System.out.println("o numero é impar " + numero);
+        }
+
+
+    }
+    
+}
